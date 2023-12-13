43c95b7d49e1587196b8baeb2af873e7f142b14f
Rick Cooper <rick.cooper@krakenflex.com>
2023-12-13 14:30:04 +0000

    fix: non-conflicting treesitter select bindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -449,10 +449,10 @@ vim.defer_fn(function()
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
+        init_selection = '<leader>vi',
+        node_incremental = '<leader>vn',
+        scope_incremental = '<leader>vs',
+        node_decremental = '<leader>v<S-n>',
       },
     },
     textobjects = {
