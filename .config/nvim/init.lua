97144aed1fd93f706bb008a159cbb00e683a9747
Rick Cooper <rick.cooper@krakenflex.com>
2023-12-15 10:16:01 +0000

    improvement: shortcut for netrw

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -337,6 +337,8 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+vim.keymap.set('n', '<leader>vf', vim.cmd.Ex, { desc = 'Open netrw file explorer' })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
